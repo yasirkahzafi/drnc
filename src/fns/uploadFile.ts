c2d4db17dc58852c672db75c5eb0696c7127582d
yasirkahzafi <yasirkahzafi@gmail.com>
2023-06-23 11:40:35 +0900

    fix issue run out of memory when sendFile

---
+++ src/fns/uploadFile.ts
@@ -0,0 +1,54 @@
+import fs from "node:fs";
+import { basename } from "node:path";
+import bigInt from "big-integer";
+import { Api } from "telegram";
+import { generateRandomBytes, readBigIntFromBuffer } from "telegram/Helpers.js";
+import { getAppropriatedPartSize } from "telegram/Utils.js";
+import bot from "../bot.js";
+
+type ProgressCallback = (chunkLength: number, uploaded: number, total: number) => void;
+
+export default async function uploadFile(
+  filePath: string,
+  progressCallback?: ProgressCallback
+): Promise<Api.InputFileBig> {
+  const fileId = readBigIntFromBuffer(generateRandomBytes(8), true, true);
+  const fileName = basename(filePath);
+  const fileSize = fs.statSync(filePath).size;
+  const defaultChunkSize = getAppropriatedPartSize(bigInt(fileSize)) * 1024;
+  const fileTotalParts = Math.ceil(fileSize / defaultChunkSize);
+  const file = fs.openSync(filePath, "r");
+
+  let partCount = 0;
+  let remainingSize = fileSize;
+
+  while (remainingSize > 0) {
+    const position = partCount * defaultChunkSize;
+    const chunkSize = remainingSize > defaultChunkSize ? defaultChunkSize : remainingSize;
+    const buffer = Buffer.alloc(chunkSize);
+
+    remainingSize -= chunkSize;
+    fs.readSync(file, buffer, { position, length: chunkSize });
+
+    await bot.invoke(
+      new Api.upload.SaveBigFilePart({
+        fileId,
+        filePart: partCount,
+        fileTotalParts,
+        bytes: buffer,
+      })
+    );
+    if (progressCallback) {
+      progressCallback(chunkSize, position, fileSize);
+    }
+    partCount++;
+  }
+  const uploadedFile = new Api.InputFileBig({
+    id: fileId,
+    parts: partCount,
+    name: fileName,
+  });
+  fs.closeSync(file);
+
+  return uploadedFile;
+}
--- src/tasks/downloadFileTask.ts
+++ src/tasks/downloadFileTask.ts
@@ -2,6 +2,7 @@ import { NewMessageEvent } from "telegram/events/index.js";
 import downloadFile from "../fns/downloadFile.js";
 import formatBytes from "../fns/formatBytes.js";
 import bot from "../bot.js";
+import uploadFile from "../fns/uploadFile.js";
 
 export default async function downloadFileTask(event: NewMessageEvent) {
   const message = event.message;
@@ -31,18 +32,17 @@ export default async function downloadFileTask(event: NewMessageEvent) {
       });
     } catch {}
   });
-  await bot.sendFile(event.chatId, {
-    file: filePath,
-    progressCallback(progress) {
-      if (inDebounce) return;
-      inDebounce = true;
-      setTimeout(() => (inDebounce = false), updateDelay);
+  const uploadedFile = await uploadFile(filePath, (chunkLength, downloaded, total) => {
+    if (inDebounce) return;
+    inDebounce = true;
+    setTimeout(() => (inDebounce = false), updateDelay);
 
-      const percentNumber = Math.round(progress * 100);
-      try {
-        replyMessage.edit({ text: `Uploading ${percentNumber}%` });
-      } catch {}
-    },
+    try {
+      replyMessage.edit({
+        text: `Uploading ${formatBytes(downloaded)} / ${formatBytes(total)}`,
+      });
+    } catch {}
   });
   await replyMessage.delete({ revoke: true });
+  await bot.sendFile(event.chatId, { file: uploadedFile });
 }
