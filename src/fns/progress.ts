2b08f69704c5e6070f2c3ead3a2c188cdffd08f8
yasirkahzafi <yasirkahzafi@gmail.com>
2023-06-24 23:52:12 +0900

    safe try catch

--- src/fns/progress.ts
+++ src/fns/progress.ts
@@ -1,13 +1,15 @@
 import globalState from "../services/globalState.js";
 import getProgressText from "./getProgressText.js";
 
-export default function progress() {
+export default async function progress() {
   const progressMessage = globalState.progressMessage;
   if (!progressMessage) return;
 
   try {
     const text = getProgressText();
     if (!text) return progressMessage.delete({ revoke: true });
-    progressMessage.edit({ text });
-  } catch {}
+    await progressMessage.edit({ text });
+  } catch (err) {
+    console.error((err as Error).message);
+  }
 }
--- src/tasks/downloadFileTask.ts
+++ src/tasks/downloadFileTask.ts
@@ -7,32 +7,36 @@ import { getFileName } from "../fns/getFileName.js";
 import getProgressText from "../fns/getProgressText.js";
 
 export default async function downloadFileTask(event: NewMessageEvent) {
-  const message = event.message;
-  const chatId = event.chatId;
-  const url = message.text.split(" ")[1];
+  try {
+    const message = event.message;
+    const chatId = event.chatId;
+    const url = message.text.split(" ")[1];
 
-  if (!chatId) {
-    return;
+    if (!chatId) {
+      return;
+    }
+    if (!url) {
+      return message.reply({ message: "Mana linknya cuy" });
+    }
+    const tasks = globalState.tasks;
+    const fileName = getFileName(url);
+
+    await globalState.progressMessage?.delete({ revoke: true });
+    tasks.set(fileName, { fileName, type: "prepare", currentState: 0, total: 0 });
+
+    const replyMessage = await message.reply({ message: getProgressText() });
+    globalState.progressMessage = replyMessage;
+
+    const filePath = await downloadFile(url, (chunkLength, downloaded, total) => {
+      tasks.set(fileName, { fileName, type: "download", currentState: downloaded, total });
+    });
+    const uploadedFile = await uploadFile(filePath, (chunkLength, uploaded, total) => {
+      tasks.set(fileName, { fileName, type: "upload", currentState: uploaded, total });
+    });
+    tasks.delete(fileName);
+
+    await bot.sendFile(event.chatId, { file: uploadedFile });
+  } catch (err) {
+    console.error((err as Error).message);
   }
-  if (!url) {
-    return message.reply({ message: "Mana linknya cuy" });
-  }
-  const tasks = globalState.tasks;
-  const fileName = getFileName(url);
-
-  await globalState.progressMessage?.delete({ revoke: true });
-  tasks.set(fileName, { fileName, type: "prepare", currentState: 0, total: 0 });
-
-  const replyMessage = await message.reply({ message: getProgressText() });
-  globalState.progressMessage = replyMessage;
-
-  const filePath = await downloadFile(url, (chunkLength, downloaded, total) => {
-    tasks.set(fileName, { fileName, type: "download", currentState: downloaded, total });
-  });
-  const uploadedFile = await uploadFile(filePath, (chunkLength, uploaded, total) => {
-    tasks.set(fileName, { fileName, type: "upload", currentState: uploaded, total });
-  });
-  tasks.delete(fileName);
-
-  await bot.sendFile(event.chatId, { file: uploadedFile });
 }
